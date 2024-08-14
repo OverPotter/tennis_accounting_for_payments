8ca21fd83b9c93750f9bf2a8d965fd862a54c4a5
QdMiracle <qdmiracle21@gmail.com>
2024-08-15 00:49:29 +0300

    refactoring payment command

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,7 +1,7 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 
-from src.handlers.help.help_handler import HelpCommandHandler
+from src.constants_text import HELP_TEXT
 from src.utils.checking_permissions import checking_permissions
 
 router = Router()
@@ -10,5 +10,4 @@ router = Router()
 @router.message(Command("help"))
 @checking_permissions
 async def send_help(message: types.Message):
-    handler = HelpCommandHandler()
-    await handler.handle(message=message)
+    await message.answer(HELP_TEXT)
