16c5f6fa198953defbc0d7a3a5939fab65d9123b
QdMiracle <qdmiracle21@gmail.com>
2024-08-16 01:25:43 +0300

    ref

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,5 +1,5 @@
 from src.commands.add_client import router as add_client_router
-from src.commands.add_payments import router as add_payment_router
+from src.commands.add_payments import router as add_payments_router
 from src.commands.help import router as help_router
 from src.commands.processing_user_response import (
     router as processing_user_response_router,
@@ -8,5 +8,5 @@ from src.dispatcher.dispatcher import dp
 
 dp.include_router(help_router)
 dp.include_router(add_client_router)
-dp.include_router(add_payment_router)
+dp.include_router(add_payments_router)
 dp.include_router(processing_user_response_router)
