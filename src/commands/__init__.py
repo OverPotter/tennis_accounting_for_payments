7ce6573755dccc144ecaeaf486be79f15bd071e1
Vlad Ratnikov <iamratnikov@yandex.by>
2024-08-16 21:06:50 +0300

    ref

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,7 +1,7 @@
 from src.commands.add_client import router as add_client_router
 from src.commands.add_payments import router as add_payment_router
-from src.commands.add_visits import router as add_visits_router
 from src.commands.add_payments import router as add_payments_router
+from src.commands.add_visits import router as add_visits_router
 from src.commands.help import router as help_router
 from src.commands.processing_user_response import (
     router as processing_user_response_router,
