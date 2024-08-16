1f5c7531ffaf7ee244b1570811f9546d754edaa9
QdMiracle <qdmiracle21@gmail.com>
2024-08-16 16:26:58 +0300

    added checking_permissions

--- src/commands/add_payments.py
+++ src/commands/add_payments.py
@@ -2,11 +2,13 @@ from aiogram import Router, types
 from aiogram.filters import Command
 
 from src.constants_text import TEXT_OF_MESSAGE_FOR_ADD_PAYMENTS_REQUEST
+from src.utils.checking_permissions import checking_permissions
 
 router = Router()
 
 
 @router.message(Command("add_payments"))
+@checking_permissions
 async def add_payment(message: types.Message):
     await message.answer(
         TEXT_OF_MESSAGE_FOR_ADD_PAYMENTS_REQUEST,
