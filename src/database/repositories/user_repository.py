f2b46badb8e25db58689ae5cdb0607bdeebd3fd6
Vlad Ratnikov <iamratnikov@yandex.by>
2024-08-21 14:29:51 +0300

    number_of_tennis_training_available

--- src/database/repositories/user_repository.py
+++ src/database/repositories/user_repository.py
@@ -8,7 +8,9 @@ from src.database.repositories.absctract_repository import AbstractRepository
 class ClientRepository(AbstractRepository[ClientModel]):
     _model = ClientModel
 
-    async def get_user_with_training_remains(self, client_name: str):
+    async def get_user_with_number_of_tennis_training_available(
+        self, client_name: str
+    ):
         query = (
             select(ClientModel)
             .options(joinedload(ClientModel.number_of_trainings_available))
--- src/services/get_remains_service/abc.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractGetRemainsService(ABC):
-    @abstractmethod
-    async def get_client_remains(self, client_name: str) -> int: ...
