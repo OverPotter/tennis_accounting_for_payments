7d2d1818a514114166de0f8b33a7885177c1c0e2
Vlad Ratnikov <136388186+izzy-ratnikov@users.noreply.github.com>
2024-08-21 20:49:41 +0300

    Feature/edit number of training (#24)
    * get_user_with_training_remains
    
    * number_of_tennis_training_available
    
    * edit
    
    ---------
    
    Co-authored-by: Vlad Ratnikov <iamratnikov@yandex.by>
    Co-authored-by: Arthur <qdmiracle21@gmail.com>

--- src/services/get_number_of_tennis_training_available_service/repository.py
+++ src/services/get_number_of_tennis_training_available_service/repository.py
@@ -1,6 +1,3 @@
-from src.database.repositories.number_of_tennis_training_available_repository import (
-    NumberOfTennisTrainingAvailableRepository,
-)
 from src.database.repositories.user_repository import ClientRepository
 from src.exceptions.entity_exceptions import EntityDoesntExistException
 from src.services.get_number_of_tennis_training_available_service.abc import (
@@ -14,12 +11,8 @@ class RepositoryGetNumberOfTennisTrainingAvailableService(
     def __init__(
         self,
         client_repository: ClientRepository,
-        number_of_tennis_training_available_repository: NumberOfTennisTrainingAvailableRepository,
     ):
         self._client_repository = client_repository
-        self._number_of_tennis_training_available_repository = (
-            number_of_tennis_training_available_repository
-        )
 
     async def get_client_number_of_tennis_training_available(
         self, client_name: str
@@ -35,7 +28,4 @@ class RepositoryGetNumberOfTennisTrainingAvailableService(
                 entity_name="client",
             )
 
-        total_trainings = sum(
-            t.number_of_training for t in client.number_of_trainings_available
-        )
-        return total_trainings
+        return client
