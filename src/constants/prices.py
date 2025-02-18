99c56118fa1ce01145b10c0e5c8a28e556b98af5
Arthur <qdmiracle21@gmail.com>
2025-02-18 13:53:22 +0300

    update price

--- src/constants/prices.py
+++ src/constants/prices.py
@@ -1,9 +1,9 @@
 from src.schemas.enums.training_types import TrainingTypesEnum
 
 PRICE_LIST = {
-    45.0: (1, TrainingTypesEnum.GROUP_TRAINING),
-    160.0: (4, TrainingTypesEnum.GROUP_TRAINING),
-    310.0: (8, TrainingTypesEnum.GROUP_TRAINING),
-    55.0: (1, TrainingTypesEnum.SPLIT_TRAINING),
-    110.0: (1, TrainingTypesEnum.INDIVIDUAL_TRAINING),
+    50.0: (1, TrainingTypesEnum.GROUP_TRAINING),
+    180.0: (4, TrainingTypesEnum.GROUP_TRAINING),
+    350.0: (8, TrainingTypesEnum.GROUP_TRAINING),
+    70.0: (1, TrainingTypesEnum.SPLIT_TRAINING),
+    130.0: (1, TrainingTypesEnum.INDIVIDUAL_TRAINING),
 }
