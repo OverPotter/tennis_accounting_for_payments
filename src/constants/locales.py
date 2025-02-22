a22a0b76a8bc147a3200ae7b4b58135aa3cb4ad5
Arthur <qdmiracle21@gmail.com>
2025-02-22 17:54:35 +0300

    added locales

---
+++ src/constants/locales.py
@@ -0,0 +1,5 @@
+LOCALES = {
+    "RU": "ru_RU.UTF-8",
+    "BY": "be_BY.UTF-8",
+    "EN": "en_US.UTF-8",
+}
--- src/tg_main.py
+++ src/tg_main.py
@@ -2,13 +2,14 @@ import asyncio
 import locale
 
 import src.commands  # noqa: F401
+from src.constants.locales import LOCALES
 from src.dispatcher.dispatcher import bot, dp
 from src.sentry.sentry import init_sentry
 
 
 async def main():
     init_sentry()
-    locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
+    locale.setlocale(locale.LC_TIME, LOCALES["RU"])
 
     await dp.start_polling(bot, skip_updates=True)
 
